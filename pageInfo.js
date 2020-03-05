3b42dd5738da412d77749d1f403b68463c83858c
Young-ah Kim <59656877+duddk19897@users.noreply.github.com>
2020-03-04 23:02:50 -0700

    Add files via upload

---
+++ pageInfo.js
@@ -0,0 +1,24 @@
+const pageInfo = {
+    drink: {
+      name: "drink",
+      title: 'drink',
+      description: 'A drink (or beverage) is a liquid intended for human consumption.'
+    },
+    water: {
+      name: "water",
+      title: 'water',
+      description: 'People have to drink water on a daily basis'
+    },
+    coffee: {
+      name: "coffee",
+      title: 'coffee',
+      description: 'I need a coffee',
+    },
+    tea: {
+      name: "tea",
+      title: 'tea',
+      description: 'I prefer tea though...',
+    }
+  }
+  
+  module.exports = pageInfo;
